1f1f7e8518773d20a47a0b209c61de6f61f7d99e
ckc2007 <ckc2007@gmail.com>
2023-05-15 20:40:43 -0400

    adding inquirer options switch statements

--- server.js
+++ server.js
@@ -2,20 +2,28 @@ const mysql = require("mysql2");
 
 const inquirer = require("inquirer");
 
-inquirer.createPromptModule([
-  {
-    type: "list",
-    name: "option",
-    message: "what would you like to do?",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "Update an employee role",
-      "Exit",
-    ],
-  },
-]);
+inquirer
+  .createPromptModule([
+    {
+      type: "list",
+      name: "option",
+      message: "what would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit",
+      ],
+    },
+  ])
+  .then((response) => {
+    switch (response.option) {
+      case "View all departments":
+        //   view option
+        break;
+    }
+  });
