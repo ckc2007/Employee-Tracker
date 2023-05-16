7f1f37091e5e481fcba6cc16604b92c26e8fdd56
ckc2007 <ckc2007@gmail.com>
2023-05-15 20:46:27 -0400

    adding express depency and functionality for server

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
-
 const mysql = require("mysql2");
-
 const inquirer = require("inquirer");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL Username
+    user: "root",
+    // TODO: Add MySQL Password
+    password: "",
+    database: "hr_db",
+  },
+  console.log(`Connected to the hr_db database.`)
+);
 
 inquirer
   .createPromptModule([
