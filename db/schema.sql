1c0c7861a389dcef2c2145bf9e4fab926293f808
ckc2007 <ckc2007@gmail.com>
2023-05-15 20:26:35 -0400

    creating employee table and content

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,12 @@ CREATE TABLE role (
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
