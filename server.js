8fe1ff3299004fa52931776de26bcb2f84949f3e
ckc2007 <ckc2007@gmail.com>
2023-05-15 20:35:17 -0400

    adding inquirer dependency to server

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
 const mysql = require("mysql2");
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL Username
-      user: 'root',
-      // TODO: Add MySQL Password
-      password: '',
-      database: 'books_db'
-    },
-    console.log(`Connected to the books_db database.`)
-  );
-
-  
+const inquirer = require("inquirer");
