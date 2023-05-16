721b7f8353a65cdbcf6ebcbff5e9c6a48e065082
ckc2007 <ckc2007@gmail.com>
2023-05-15 22:20:56 -0400

    seed data errors - fixing

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
-CREATE hr_db;
+CREATE DATABASE hr_db;
 
 USE hr_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(50)
 );
 
 CREATE TABLE role (
   id INT PRIMARY KEY,
-  title VARCHAR(30),
+  title VARCHAR(50),
   salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
@@ -17,8 +17,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
