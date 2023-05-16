9b264255750890ba14f77dce9d40c6aef666286d
ckc2007 <ckc2007@gmail.com>
2023-05-16 09:47:29 -0400

    adding employee role id to update selection list

--- server.js
+++ server.js
@@ -154,10 +154,10 @@ function runPrompt() {
           //   keep in mind that if an employee is a manager themselves, then they will not have a manager, hence a null value
           db.query(
             "SELECT * FROM employee WHERE manager_id IS NULL",
-            (err, results) => {
+            (err, managerResults) => {
               if (err) throw err;
               // create an array of manager names
-              const managerArr = results.map(
+              const managerArr = managerResults.map(
                 (manager) => `${manager.first_name} ${manager.last_name}`
               );
               // prompt for input of new employee
@@ -193,9 +193,12 @@ function runPrompt() {
                   },
                 ])
                 .then((answer) => {
-                  const roleId = results[roleArr.indexOf(answer.role)].id;
+                  console.log(roleArr.indexOf(answer.role));
+                  console.log(managerResults[roleArr.indexOf(answer.role)]);
+                  const roleId =
+                    results[roleArr.indexOf(answer.role)].department_id;
                   const managerId =
-                    results[managerArr.indexOf(answer.manager)].id;
+                    managerResults[managerArr.indexOf(answer.manager)].id;
 
                   // add new employee to employee
                   db.query(
@@ -219,46 +222,60 @@ function runPrompt() {
         });
         break;
       case "Update an employee role":
-        // list all employees in an array
-        db.query("SELECT * FROM employee", (err, results) => {
+        // Retrieve roles from the role table
+        db.query("SELECT * FROM role", (err, results) => {
           if (err) throw err;
-          // new array
-          const employeeArr = results.map(
-            (employee) => `${employee.first_name} ${employee.last_name}`
-          );
-          // select the employee to update
-          inquirer
-            .prompt([
-              {
-                type: "list",
-                message: "Please select an employee to update:",
-                choices: employeeArr,
-                name: "employee",
-              },
-              {
-                type: "input",
-                message: "Pleas enter the employee's new role ID:",
-                name: "role_id",
-              },
-            ])
-            .then((answer) => {
-              // get the employees id based on the selection
-              const employeeId =
-                results[employeeArr.indexOf(answer.employee)].id;
-              // update the employee role in employee table
-              db.query(
-                "UPDATE employee SET role_id = ? WHERE id = ?",
-                [answer.role_id, employeeId],
-                (err) => {
-                  if (err) throw err;
-                  console.clear();
-                  console.log("Employee role updated successfully!");
-                  runPrompt();
-                }
-              );
-            });
+          // Create an array of role choices with role name and ID
+          const roleChoices = results.map((role) => ({
+            name: `${role.title} (ID: ${role.id})`,
+            value: role.id,
+          }));
+
+          // List all employees in an array
+          db.query("SELECT * FROM employee", (err, results) => {
+            if (err) throw err;
+            // New array of employee choices
+            const employeeArr = results.map(
+              (employee) => `${employee.first_name} ${employee.last_name}`
+            );
+
+            // Select the employee to update and enter the new role ID
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  message: "Please select an employee to update:",
+                  choices: employeeArr,
+                  name: "employee",
+                },
+                {
+                  type: "list",
+                  message: "Please select the employee's new role:",
+                  choices: roleChoices,
+                  name: "role_id",
+                },
+              ])
+              .then((answer) => {
+                // Get the employee's ID based on the selection
+                const employeeId =
+                  results[employeeArr.indexOf(answer.employee)].id;
+
+                // Update the employee role in the employee table
+                db.query(
+                  "UPDATE employee SET role_id = ? WHERE id = ?",
+                  [answer.role_id, employeeId],
+                  (err) => {
+                    if (err) throw err;
+                    console.clear();
+                    console.log("Employee role updated successfully!");
+                    runPrompt();
+                  }
+                );
+              });
+          });
         });
         break;
+
       case "Exit":
         // handle exit option
         process.exit();
