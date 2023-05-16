a656130adfd14c8dbc73c759cd51b2b023523caa
ckc2007 <ckc2007@gmail.com>
2023-05-15 21:15:31 -0400

    adding view all employees option handler

--- server.js
+++ server.js
@@ -66,6 +66,17 @@ inquirer
         break;
       case "View all employees":
         //view all employees option
+        db.query(
+          "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager " +
+            "FROM employee " +
+            "LEFT JOIN role ON employee.role_id = role.id " +
+            "LEFT JOIN department ON role.department_id = department.id " +
+            "LEFT JOIN employee manager ON employee.manager_id = manager.id",
+          (err, results) => {
+            if (err) throw err;
+            console.table(results);
+          }
+        );
         break;
       case "Add a department":
         // add a department option
