6b080ddfb8b6f9cc43e8a0bcc5ec9186be5fea33
ckc2007 <ckc2007@gmail.com>
2023-05-15 20:38:31 -0400

    adding inquirer options

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const mysql = require("mysql2");
 
 const inquirer = require("inquirer");
+
+inquirer.createPromptModule([
+  {
+    type: "list",
+    name: "option",
+    message: "what would you like to do?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Exit",
+    ],
+  },
+]);
