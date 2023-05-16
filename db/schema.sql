9d760d6f69ba9eaf40540c443004e249427ef449
ckc2007 <ckc2007@gmail.com>
2023-05-15 20:25:06 -0400

    creating role table and contents

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE department (
     name VARCHAR(30)
 );
 
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
