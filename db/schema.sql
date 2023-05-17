f5b9e94991b0deab46a4a205a9325ab8be8d8921
ckc2007 <ckc2007@gmail.com>
2023-05-16 21:12:22 -0400

    changed VARCHAR 30 per instructions

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,12 @@ USE hr_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  title VARCHAR(50),
+  title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
@@ -18,8 +18,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
